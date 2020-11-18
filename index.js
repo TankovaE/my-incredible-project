e8c33e18d6b1fef785f2b7c3e14114b0a7bf13aa
Ekaterina T <eitnkv@gmail.com>
2020-11-18 23:32:16 +0300

    added handlebars engine for rendering dinamic pages

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const path = require('path');
+//подключаем шаблонизатор
+const exphbs = require('express-handlebars');
+
 //аналог объекта server
 const app = express();
+
+//объект конфигурации handlebars
+const hbs = exphbs.create({
+    //стандартный лейаут
+    //смотрит в папку с зарезервированным именем layouts
+    defaultLayout: 'main',
+    //сокращенное название модуля, для уоббства
+    extname: 'hbs'
+});
+
+//для того чтобы зарегистрировать данный модуль как движок для рендеринга html страниц
+//первый параметр - имя, второй - значение
+app.engine('hbs', hbs.engine);
+//указываем, какой engine мы будем использовать
+app.set('view engine', 'hbs');
+//указывем, где хранятся шаблоны
+//параметры - название и папка
+app.set('views', 'views');
+
 
 //базовый метод который позволяет обрабатывать различные запросы
 //первый параметр - адрес страницы, второй - это handler, который принимает 3 прараметра
 //здесь мы отправляем в ответе на запрос get "/" статус 200 и отдаем страницу index.html
 app.get('/', (req, res, next) => {
-    res.status(200);
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+    //так мы рендерим страницы без hbs
+    // res.status(200);
+    // res.sendFile(path.join(__dirname, 'views', 'index.html'))
+
+    //так мы рендерим страницы с движком hbs
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
