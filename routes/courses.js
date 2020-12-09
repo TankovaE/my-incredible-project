965ff4e10f79c9d3ab5367d2f815bd8d9efab712
Ekaterina T <eitnkv@gmail.com>
2020-12-09 22:52:52 +0300

    render items from database on client

--- routes/courses.js
+++ routes/courses.js
@@ -1,14 +1,16 @@
 const { Router } = require('express');
-
+const Course = require('../models/course');
 const router = Router();
 
 // с помощью router можно описывать конкретные роуты
 // здесь первый параметр "/", так как мы указали префикс в файле index.js для каждого роута
 // app.use('/courses', coursesRoutes)
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const courses = await Course.getAll();
     res.render('courses', {
         title: 'Courses', 
         isCourses: true,
+        courses,
     });
 });
 
