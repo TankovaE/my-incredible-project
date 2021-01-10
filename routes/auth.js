97d8dd68a035d46541f270139169bd9352db0ca6
Ekaterina T <eitnkv@gmail.com>
2021-01-10 23:38:15 +0300

    sign in

--- routes/auth.js
+++ routes/auth.js
@@ -12,18 +12,35 @@ router.get('/login', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    const user = await User.findById('5fe8cb422cce6422f4b59419');
-    // session берется из пакета для работы с сессиями express-session 
-    // isAuthenticated - наша переменная
-    req.session.user = user;
-    req.session.isAuthenticated = true;
-    req.session.save(err => {
-        if (err) {
-            throw err
+    try {
+        const { email, password } = req.body;
+        const candidate = await User.findOne({ email });
+
+        if (candidate) {
+            const areSame = password === candidate.password;
+            if (areSame) {
+                // если пароль верный
+                const user = candidate;
+                // session берется из пакета для работы с сессиями express-session 
+                // isAuthenticated - наша переменная
+                req.session.user = user;
+                req.session.isAuthenticated = true;
+                req.session.save(err => {
+                    if (err) {
+                        throw err
+                    }
+                    // делаем редирект только если авторизация прошла ок
+                    res.redirect('/')
+                })
+            } else  {
+                res.redirect('/auth/login#login');
+            }
+        } else {
+            res.redirect('/auth/login#login');
         }
-        // делаем редирект только если авторизация прошла ок
-        res.redirect('/')
-    })
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 // очищаем сессию и редиректим на главную
@@ -37,13 +54,15 @@ router.get('/logout', async (req, res) => {
 
 router.post('/register', async (req, res) => {
     try {
-        //берез из формы данные, которые ввел пользователь при регистрации
+        // берем из формы данные, которые ввел пользователь при регистрации
         const { email, name, password, repeat } = req.body;
+        // ищем в базе юзера с такие емейлом
         const candidate = await User.findOne({ email });
 
         if (candidate) {
             res.redirect('/auth/login#register')
         } else {
+            // добавляем нового юзера только если email не занят
             const user = new User({
                 email, name, password, cart: { items: [] }
             });
