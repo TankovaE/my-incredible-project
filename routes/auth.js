48b183a19bfbe2d5af50cf34e24346d74c15f482
Ekaterina T <eitnkv@gmail.com>
2021-02-23 22:42:04 +0300

    async validation

--- routes/auth.js
+++ routes/auth.js
@@ -81,38 +81,35 @@ router.get('/logout', async (req, res) => {
 router.post('/register', registerValidators, async (req, res) => {
     try {
         // берем из формы данные, которые ввел пользователь при регистрации
-        const { email, name, password, confirm } = req.body;
+        const { email, name, password } = req.body;
 
         // проверяем, есть ли ошибка от валидатора
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
+            // метод из библиотеки flash, который позволяет с помощью сессии делать транспортировку определенныx ошибок между запросами
             req.flash('registerError', errors.array()[0].msg);
             // 422 - ошибка валидации
             return res.status(422).redirect('/auth/login#register');
         }
 
         // ищем в базе юзера с такие емейлом
-        const candidate = await User.findOne({ email });
+        // const candidate = await User.findOne({ email });
 
-        if (candidate) {
-            // метод из библиотеки flash, который позволяет с помощью сессии делать транспортировку определенныx ошибок между запросами
-            req.flash('registerError', 'User with this email is exist')
-            res.redirect('/auth/login#register');
-        } else {
-            // hash - асинхронный метод, который возвращает промис
-            // он помогает нам зашифровать пароль
-            // так password : "$2a$10$iR0mSyxUSMw85Cb7XBNOj.aOTSvQGa8VSc.zioHcAolz7aWBd7X5m" он будет выглядеть в базе в зашифрованном виде
-            const hashPassword = await bcrypt.hash(password, 10)
-            // добавляем нового юзера только если email не занят
-            const user = new User({
-                email, name, password: hashPassword, cart: { items: [] }
-            });
-            await user.save();
-            // отправляем пользователю письмо с информацией о регистрауии
-            // будет отправляться в фоновом режиме, после редиректа, чтобы пользователь не ждал
-            await transporter.sendMail(regEmail(email));
-            res.redirect('/auth/login#login');
-        }
+
+        // hash - асинхронный метод, который возвращает промис
+        // он помогает нам зашифровать пароль
+        // так password : "$2a$10$iR0mSyxUSMw85Cb7XBNOj.aOTSvQGa8VSc.zioHcAolz7aWBd7X5m" он будет выглядеть в базе в зашифрованном виде
+        const hashPassword = await bcrypt.hash(password, 10)
+        // добавляем нового юзера только если email не занят
+        const user = new User({
+            email, name, password: hashPassword, cart: { items: [] }
+        });
+        await user.save();
+        // отправляем пользователю письмо с информацией о регистрауии
+        // будет отправляться в фоновом режиме, после редиректа, чтобы пользователь не ждал
+        await transporter.sendMail(regEmail(email));
+        res.redirect('/auth/login#login');
+        
 
     } catch (e) {
         console.log(e);
--- utils/validators.js
+++ utils/validators.js
@@ -1,9 +1,19 @@
 const { body } = require('express-validator/check');
+const User = require('../models/user');
 
 // body проверфет body запроса, есть и другие валидаторы
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Enter correct email'),
+    body('email').isEmail().withMessage('Enter correct email').custom(async (value, { req }) => {
+        try {
+            const user = await User.findOne({ email: value });
+            if (user) {
+                return Promise.reject('User with this email is exist')
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }),
     // isAlphanumeric проверет, чтобы пароль состоял из цифр или букв и был латинский
     body('password', 'Minimum password length 6 characters').isLength({ min: 3, max: 56 }).isAlphanumeric(),
     body('confirm').custom((value, { req }) => {
