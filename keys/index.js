69bdd1a35bdb11eb9bc72a3de05e4eded0969bce
Ekaterina T <eitnkv@gmail.com>
2021-02-25 00:27:33 +0300

    keys protection for NODE_ENV === production

--- keys/index.js
+++ keys/index.js
@@ -1,8 +1,5 @@
-// объект конфигурации для приложения
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://eitnkv:yKyonP8JZCOxEmye@cluster0.vdlvu.mongodb.net/shop',
-    SESSION_SECRET: 'some secret',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'eitnkv@gmail.com',
-    BASE_URL: 'http://localhost:3000',
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod');
+} else {
+    module.exports = require('./keys.dev');
 }
